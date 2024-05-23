9949f204e27e805ec1587516e2f01c7a904328d1
aanafreita <129406161+aanafreita@users.noreply.github.com>
2024-05-23 09:44:07 -0300

    23/05

---
+++ script.js
@@ -0,0 +1,16 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaperguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaresultado = document.querySelector('.caixa-resultado');
+const textoresultado = document.querySelector('.texto-reaultado');
+
+ const perguntas = [
+  {
+    enunciado:"pergunta 1",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  },
+  {
+    enunciado:"pergunta 2",
+    alternativas: ["Alternativa 1 ", "Alternativa 2"],
+  },
+]
